40ab798bef604a1887514debd4d49e2d6ab477be
techhandoo <krishhandoo@gmail.com>
2026-09-11 23:59:48 +0530

    added missing import

--- backend/src/main/java/com/eventmanager/service/EventService.java
+++ backend/src/main/java/com/eventmanager/service/EventService.java
@@ -18,6 +18,7 @@ import com.eventmanager.security.InputSanitizer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
